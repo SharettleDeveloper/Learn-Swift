802e7e877ae25b40a6b104482cb5825d3f42e0a2
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2024-09-30 02:12:13 +0900

    Book-OshigeYoshiyuki

---
+++ Book-OshigeYoshiyuki-2022/Book-OshigeYoshiyuki-2022/Book_OshigeYoshiyuki_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Book_OshigeYoshiyuki_2022App.swift
+//  Book-OshigeYoshiyuki-2022
+//
+//  Created by RAIMU MIWA on 2024/09/30.
+//
+
+import SwiftUI
+
+@main
+struct Book_OshigeYoshiyuki_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Book-OshigeYoshiyuki-2022/Book-OshigeYoshiyuki-2022/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Book-OshigeYoshiyuki-2022
+//
+//  Created by RAIMU MIWA on 2024/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
