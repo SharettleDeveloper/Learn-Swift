adec2099723f04d3eb6f3b0e0ae22338d81a9c42
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2024-09-30 02:22:18 +0900

    readme

---
+++ Book-OshigeYoshiyuki-2022/Book-OshigeYoshiyuki-2022/Chapter3/Chapter3-1.swift
@@ -0,0 +1,18 @@
+//
+//  Chapter3-1.swift
+//  Book-OshigeYoshiyuki-2022
+//
+//  Created by RAIMU MIWA on 2024/09/30.
+//
+
+import SwiftUI
+
+struct Chapter3_1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Chapter3_1()
+}
