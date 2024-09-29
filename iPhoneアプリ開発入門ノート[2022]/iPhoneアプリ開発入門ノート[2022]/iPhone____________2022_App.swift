71fd80e90960a5741da9db8be48a772712a70de8
sharettleDeveloper <SharettleDeveloper@users.noreply.github.com>
2024-09-30 00:16:14 +0900

    start Swift 01

---
+++ iPhoneアプリ開発入門ノート[2022]/iPhoneアプリ開発入門ノート[2022]/iPhone____________2022_App.swift
@@ -0,0 +1,17 @@
+//
+//  iPhone____________2022_App.swift
+//  iPhoneアプリ開発入門ノート[2022]
+//
+//  Created by RAIMU MIWA on 2024/09/30.
+//
+
+import SwiftUI
+
+@main
+struct iPhone____________2022_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
